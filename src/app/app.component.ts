6d11ce043a1605445b1a1959691751380816cc33
ajitmourya7 <mouryaa33@gmail.com>
2019-09-30 13:08:14 +0530

    Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'form';
+  form: any;
+
+  constructor() {
+    this.form = {
+      firstName : '',
+      middleName : '',
+      lastName : '',
+      currentAddress : '',
+      email : '',
+      mobileNumber : '',
+      position : '',
+      fileData : ''
+    };
+  }
+  submit() {
+    console.log(this.form);
+  }
 }
